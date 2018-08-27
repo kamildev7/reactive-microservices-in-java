c73d69a737ab978db97c908416e178739c8ed2b3
kamildev7 <k.93kamil@gmail.com>
2018-08-27 21:30:51 +0200

    Created hello-microservice-message (Message-Based Microservice)

---
+++ microservices/hello-microservice-message/src/main/java/io/vertx/micro/message/HelloMicroservice.java
@@ -0,0 +1,12 @@
+package io.vertx.micro.message;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}

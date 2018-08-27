198ed72c445506bd9f6f0730736bb5fb56afd5a1
kamildev7 <k.93kamil@gmail.com>
2018-08-27 20:39:46 +0200

    Created microservice consumer

--- microservices/hello-consumer-microservice-http/src/main/java/io/vertx/micro/http/HelloConsumerMicroservice.java
+++ microservices/hello-consumer-microservice-http/src/main/java/io/vertx/micro/http/HelloConsumerMicroservice.java
@@ -1,12 +1,41 @@
 package io.vertx.micro.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 
 public class HelloConsumerMicroservice extends AbstractVerticle {
 
+    private WebClient client;
+
     @Override
     public void start() {
+        client = WebClient.create(vertx);
 
+        Router router = Router.router(vertx);
+        router.get("/").handler(this::invokeMyFirstMicroservice);
+
+        vertx.createHttpServer()
+            .requestHandler(router::accept)
+            .listen(8081);
+    }
+
+    private void invokeMyFirstMicroservice(RoutingContext routingContext) {
+        HttpRequest<JsonObject> request = client
+            .get(8080, "localhost", "/vert.x")
+            .as(BodyCodec.jsonObject());
+
+        request.send(ar -> {
+            if (ar.failed()) {
+                routingContext.fail(ar.cause());
+            } else {
+                routingContext.response().end(ar.result().body().encode());
+            }
+        });
     }
 
 }

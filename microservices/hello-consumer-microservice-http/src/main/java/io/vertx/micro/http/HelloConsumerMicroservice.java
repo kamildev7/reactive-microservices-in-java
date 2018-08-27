7ed0e4fafb42cee1c8a7127918e66b8deea20f4b
kamildev7 <k.93kamil@gmail.com>
2018-08-27 21:00:19 +0200

    Added more requests to consumer

--- microservices/hello-consumer-microservice-http/src/main/java/io/vertx/micro/http/HelloConsumerMicroservice.java
+++ microservices/hello-consumer-microservice-http/src/main/java/io/vertx/micro/http/HelloConsumerMicroservice.java
@@ -1,12 +1,11 @@
 package io.vertx.micro.http;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpRequest;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.web.*;
+import io.vertx.rxjava.ext.web.client.*;
+import io.vertx.rxjava.ext.web.codec.BodyCodec;
+import rx.Single;
 
 public class HelloConsumerMicroservice extends AbstractVerticle {
 
@@ -25,17 +24,34 @@ public class HelloConsumerMicroservice extends AbstractVerticle {
     }
 
     private void invokeMyFirstMicroservice(RoutingContext routingContext) {
-        HttpRequest<JsonObject> request = client
-            .get(8080, "localhost", "/vert.x")
+
+        HttpRequest<JsonObject> request1 = client
+            .get(8080, "localhost", "/Tim")
             .as(BodyCodec.jsonObject());
 
-        request.send(ar -> {
-            if (ar.failed()) {
-                routingContext.fail(ar.cause());
-            } else {
-                routingContext.response().end(ar.result().body().encode());
-            }
-        });
-    }
+        HttpRequest<JsonObject> request2 = client
+            .get(8080, "localhost", "/Ann")
+            .as(BodyCodec.jsonObject());
 
+        Single<JsonObject> single1 = request1.rxSend()
+            .map(HttpResponse::body);
+
+        Single<JsonObject> single2 = request2.rxSend()
+            .map(HttpResponse::body);
+
+        Single
+            .zip(single1, single2, (tim, ann) -> {
+                // We have the results of both requests in Time and Ann
+                return new JsonObject()
+                    .put("Tim", tim.getString("message"))
+                    .put("Ann", ann.getString("message"));
+            })
+            .subscribe(
+                result -> routingContext.response().end(result.encodePrettily()),
+                error -> {
+                    error.printStackTrace();
+                    routingContext.response().setStatusCode(500).end(error.getMessage());
+                }
+            );
+    }
 }

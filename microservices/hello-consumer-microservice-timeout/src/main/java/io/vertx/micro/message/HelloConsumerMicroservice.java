8e65a3a573cec809ab1ed99f29f4cb116c957511
kamildev7 <k.93kamil@gmail.com>
2018-08-29 01:33:46 +0200

    Created hello-consumer-microservice-timeout project

---
+++ microservices/hello-consumer-microservice-timeout/src/main/java/io/vertx/micro/message/HelloConsumerMicroservice.java
@@ -0,0 +1,12 @@
+package io.vertx.micro.message;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloConsumerMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
